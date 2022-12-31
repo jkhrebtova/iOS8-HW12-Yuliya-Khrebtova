3101c27ffa7da9d1c18fe1fbe9456cbb0e91930a
yuliya_khrebtova <j.khrebtova1@gmail.com>
2022-12-31 18:04:06 +0400

    Update-after-review.

--- iOS8-HW12-Yuliya Khrebtova/Sources/Extension CA Layer.swift
+++ iOS8-HW12-Yuliya Khrebtova/Sources/Extension CA Layer.swift
@@ -1,32 +1,11 @@
 //
-//  Extensions.swift
+//  Extension.swift
 //  iOS8-HW12-Yuliya Khrebtova
 //
-//  Created by Julia on 25.12.2022.
+//  Created by Julia on 31.12.2022.
 //
 
 import UIKit
-
-extension TimeInterval {
-    var minuteSecondMS: String {
-        String(format:"%02d:%02d.%02d", minute, second, millisecond)
-    }
-    var secondMS: String {
-        String(format:"%02d:%02d", second, millisecond)
-    }
-    var hour: Int {
-        Int((self / 3600).truncatingRemainder(dividingBy: 3600))
-    }
-    var minute: Int {
-        Int((self / 60).truncatingRemainder(dividingBy: 60))
-    }
-    var second: Int {
-        Int(truncatingRemainder(dividingBy: 60))
-    }
-    var millisecond: Int {
-        Int((self * 100).truncatingRemainder(dividingBy: 100))
-    }
-}
 
 extension CALayer {
     func pauseAnimation() {
